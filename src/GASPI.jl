c09d7cbf1edc95b1657f70963cde55b125e94a0f
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-13 22:55:54 +0200

    First working version (`gaspi_version` works)

--- src/GASPI.jl
+++ src/GASPI.jl
@@ -1,16 +1,17 @@
 module GASPI
 
-using Preferences: load_preference, set_preferences!
+using Preferences: @load_preference, @set_preferences!
 using Reexport: @reexport
 
-include("LibGASPI.jl")
+export get_gaspi_library, set_gaspi_library!
 
-@reexport using .LibGASPI
-
-get_gaspi_library() = load_preference(GASPI, "gaspi_library", default=ENV["JULIA_GASPI_LIBRARY"])
+get_gaspi_library() = @load_preference("gaspi_library", default=ENV["JULIA_GASPI_LIBRARY"])
 
 function set_gaspi_library!(path)
-  set_preferences!(GASPI, "gaspi_library" => path, force=true)
+  @set_preferences!("gaspi_library" => path)
 end
+
+include("LibGASPI.jl")
+@reexport using .LibGASPI
 
 end # module
