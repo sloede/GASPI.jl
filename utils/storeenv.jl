6068de395372da567293086cb51047083e1201ad
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-15 22:11:52 +0200

    Add utility functions for making GASPI.jl run on clusters with `module` support (#2)
    * Add storeenv.jl and launcher.jl scripts
    
    * update README
    
    * Small fix

---
+++ utils/storeenv.jl
@@ -0,0 +1,16 @@
+using TOML
+using Pkg
+
+if length(ARGS) > 0
+  filename = ARGS[1]
+else
+  filename = "gaspi-jl-env.toml"
+end
+
+data = Dict{String,Any}("ENV" => ENV)
+data["julia_command"] = collect(Base.julia_cmd())
+data["project"] = dirname(Pkg.project().path)
+
+open(filename, "w") do io
+  TOML.print(io, data)
+end
