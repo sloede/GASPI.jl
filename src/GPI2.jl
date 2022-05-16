0163f268857a9f03c228963edc9871b4ff2dc161
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-16 11:05:48 +0200

    Add gaspi_logger for convenience

--- src/GPI2.jl
+++ src/GPI2.jl
@@ -4,7 +4,10 @@ using Preferences: load_preference, @set_preferences!
 using Reexport: @reexport
 using GPI2_jll
 
+export gaspi_logger
+
 const libGPI2 = load_preference(GPI2, "libGPI2", GPI2_jll.libGPI2)
+const gaspi_logger_executable = load_preference(GPI2, "gaspi_logger_executable", GPI2_jll.gaspi_logger_path)
 
 function use_jll_library()
   @set_preferences!("libGPI2" => GPI2_jll.libGPI2)
@@ -14,6 +17,17 @@ end
 function use_system_library(path)
   @set_preferences!("libGPI2" => path)
   @info "Using user-provided GPI-2 library. Please restart Julia for the change to take effect."
+end
+
+function gaspi_logger()
+  try
+    run(`$gaspi_logger_executable`)
+  catch InterruptException
+    if !isinteractive()
+      println("Caught Ctrl-C, stopping...")
+      exit()
+    end
+  end
 end
 
 include("LibGPI2.jl")
