5d72b110e97318a1f2eb96928909c8e14c3a8826
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-16 11:57:21 +0200

    Add gaspi_run function

--- src/GPI2.jl
+++ src/GPI2.jl
@@ -4,10 +4,11 @@ using Preferences: load_preference, @set_preferences!
 using Reexport: @reexport
 using GPI2_jll
 
-export gaspi_logger
+export gaspi_logger, gaspi_run
 
 const libGPI2 = load_preference(GPI2, "libGPI2", GPI2_jll.libGPI2)
 const gaspi_logger_executable = load_preference(GPI2, "gaspi_logger_executable", GPI2_jll.gaspi_logger_path)
+const gaspi_run_executable = load_preference(GPI2, "gaspi_run_executable", GPI2_jll.gaspi_run_path)
 
 function use_jll_library()
   @set_preferences!("libGPI2" => GPI2_jll.libGPI2)
@@ -22,6 +23,17 @@ end
 function gaspi_logger()
   try
     run(`$gaspi_logger_executable`)
+  catch InterruptException
+    if !isinteractive()
+      println("Caught Ctrl-C, stopping...")
+      exit()
+    end
+  end
+end
+
+function gaspi_run()
+  try
+    run(`$gaspi_run_executable $ARGS`)
   catch InterruptException
     if !isinteractive()
       println("Caught Ctrl-C, stopping...")
