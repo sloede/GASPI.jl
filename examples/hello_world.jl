7f0aabf0e50b2a870ee2dedb3872d619321dc832
Michael Schlottke-Lakemper <michael@sloede.com>
2022-05-19 08:40:53 +0200

    Use normal println again

--- examples/hello_world.jl
+++ examples/hello_world.jl
@@ -8,8 +8,8 @@ num = Ref{gaspi_rank_t}()
 gaspi_proc_rank(rank)
 gaspi_proc_num(num)
 
-gaspi_printf("Hello from rank %d of %d\n", rank, num)
-# Comment the previous and uncomment the following line if you do not want to use the logger
-# println("Hello from rank $(rank[]) of $(num[])")
+# gaspi_printf("Hello from rank %d of %d\n", rank, num)
+# Uncomment the previous and comment the following line if you want to use the gaspi_logger
+println("Hello from rank $(rank[]) of $(num[])")
 
 gaspi_proc_term(GASPI_BLOCK)
